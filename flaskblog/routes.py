879e9fe4f379f54a9a5bf132425c4d018c0c3aad
gia-bartlett <g.bartlett89@gmail.com>
2020-07-24 15:38:25 +0100

    added account page that display username when logged in

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -67,4 +67,4 @@ def logout():
 # create a route to user's account
 @app.route("/account")
 def account():
-
+    return render_template('account.html', title='Account')
