53c3bcfd06a356dd1d6fd8dbc83e7a417f60ef9a
gia-bartlett <g.bartlett89@gmail.com>
2020-07-24 12:09:18 +0100

    hardcoding an admin suer to test login

--- app.py
+++ app.py
@@ -39,9 +39,15 @@ def register():
         return redirect(url_for("home"))  # naming function not route
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()  # create an instance of the form
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
